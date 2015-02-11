f6bc1b51d03acd99f24f66791daf7b4158990265
Ty Overby <ty@pre-alpha.com>
2015-02-10 20:25:30 -0800

    more work in demo

--- examples/demo.rs
+++ examples/demo.rs
@@ -4,6 +4,7 @@ use chipsys::*;
 
 fn main() { unsafe {
     let gravity = cpv(0.0, -100.0);
+    let zero = cpv(0.0, 0.0);
 
     let space = cpSpaceNew();
     cpSpaceSetGravity(space, gravity);
@@ -13,5 +14,15 @@ fn main() { unsafe {
                                    0.0);
   cpShapeSetFriction(ground, 1.0);
   cpSpaceAddShape(space, ground);
+
+  let radius = 5.0;
+  let mass = 1.0;
+
+  let moment = cpMomentForCircle(mass, 0.0, radius, zero);
+
+  let ballbody = cpSpaceAddBody(space, cpBodyNew(mass, moment));
+  cpBodySetPosition(ballbody, cpv(0.0, 15.0));
+
+  let ballShape = cpSpaceAddShape(space, cpCircleShapeNew(ballbody, radius, zero));
 
 }}
